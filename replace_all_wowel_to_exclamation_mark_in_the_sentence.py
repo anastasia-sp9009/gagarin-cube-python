884f72f051b095db44fdda084b20fa3b09966d46
anastasia-sp9009 <anastasia.volychkina@gmail.com>
2023-04-22 14:43:06 +0300

    Replace all wowel to exclamation mark in the sentence kata

---
+++ replace_all_wowel_to_exclamation_mark_in_the_sentence.py
@@ -0,0 +1,3 @@
+#https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+def replace_exclamation(s):
+    return s.replace('a', '!').replace('e', '!').replace('i', '!').replace('o', '!').replace('u', '!').replace('A', '!').replace('E', '!').replace('I', '!').replace('O', '!').replace('U', '!')
